e5e61dbd177dcbdb4bbe7440aa440498406eae8d
allanino <allaninocencio@yahoo.com.br>
2015-02-20 17:50:42 -0200

    Add endpoint to test etcd communication

--- app/app.py
+++ app/app.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python
 from flask import Flask
 import sys
+import etcd
+import os
 
 app = Flask(__name__)
 app.config['NAME'] = 'def'
@@ -12,6 +14,11 @@ def hello_world():
 def hello_name():
     return 'Name is %s\n' % app.config['NAME']
 
+@app.route('/db')
+def db():
+    client = etcd.Client(host=os.environ.get('ETCD_IP', 'localhost'), port=int(os.environ.get('ETCD_PORT', 4001)))
+    return '%s' % client.read('/', recursive=True)
+
 if __name__ == '__main__':
     app.config['NAME'] = sys.argv[1]
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=5000, debug=True)
